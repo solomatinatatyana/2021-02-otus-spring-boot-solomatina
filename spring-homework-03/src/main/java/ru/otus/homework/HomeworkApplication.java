8aa9503edfe82e3d6360ad2c4906cfa888707b1b
Tatyana Solomatina <Solomatina.T.An@sberbank.ru>
2021-03-23 22:44:59 +0300

    spring-homework-03: homework-03 is done

--- spring-homework-03/src/main/java/ru/otus/homework/HomeworkApplication.java
+++ spring-homework-03/src/main/java/ru/otus/homework/HomeworkApplication.java
@@ -2,12 +2,17 @@ package ru.otus.homework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ConfigurableApplicationContext;
+import ru.otus.homework.service.survey.SurveyService;
+
+import java.io.IOException;
 
 @SpringBootApplication
-public class HomeworkApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(HomeworkApplication.class, args);
+public class HomeworkApplication{
+    public static void main(String[] args) throws IOException {
+        ConfigurableApplicationContext context = SpringApplication.run(HomeworkApplication.class, args);
+        SurveyService surveyService = context.getBean(SurveyService.class);
+        System.out.println(surveyService.takeASurvey());
     }
-
 }
